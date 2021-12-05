36b5710144838521c6fd15600a15e410f9e386df
sandesh jain <sandeshrjain@gmail.com>
2021-12-04 23:45:58 -0500

    relative paths

--- kcft with nn.py
+++ kcft with nn.py
@@ -19,7 +19,7 @@ import random
 class_names = ['walking', 'block', 'idle'] #can add character names here
 c = len(class_names) #used for output of the neural network
 #video = ""  #name of the video to perform detection on
-prefix = "C:/Users/Sandesh Jain/OneDrive/Documents/Acads_VT/SEM2/CV/Project/training/training/"
+prefix = "./training/"
 all_paths  = [prefix + x for x in class_names]
 # feed the nn with 100x100 images, hence resize function:
 def resizeImage(image):
@@ -158,4 +158,4 @@ def tracker(vid_name):
                 break
     cv2.destroyAllWindows()
     out.release()
-tracker("C:/Users/Sandesh Jain/Documents/Python Scripts/game1.webm")
+tracker("./game1.webm")
